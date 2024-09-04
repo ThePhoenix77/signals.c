dcec87a23f1d6feb53af0fc3725a63e0b8ffedc0
Boussaden Taha <117488861+ThePhoenix77@users.noreply.github.com>
2024-09-04 20:07:34 +0100

    Create sig.c

---
+++ sig.c
@@ -0,0 +1,61 @@
+#include "../includes/minishell.h"
+
+/*struct sigaction {
+    void (*sa_handler)(int);  Signal handler 
+    void (*sa_sigaction)(int, siginfo_t *, void *); Alternate signal handler 
+    sigset_t sa_mask;         Signal mask 
+    int sa_flags;             Flags 
+    void (*sa_restorer)(void); Restorer function
+};*/
+
+typedef struct sigaction t_sigaction;
+
+void	*ft_memset2(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	str = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		str[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+void signals_handler(int sig)
+{
+    if (sig == SIGINT)
+    {
+        printf("\nReceived SIGINT\n");
+        // rl_on_new_line();
+        // rl_replace_line("", 0);
+        // rl_redisplay();
+    }
+    else if (sig == SIGQUIT)
+        printf("\nReceived SIGQUIT\n");
+}
+
+void mask_and_catch_signals(t_sigaction *sig)
+{
+    sig->sa_handler = signals_handler;
+
+    //to use only in the signals collision
+    // sig->sa_flags = SA_RESTART; // Restart interrupted system calls
+    
+    sigemptyset(&sig->sa_mask);
+    sigaddset(&sig->sa_mask, SIGINT);
+    sigaddset(&sig->sa_mask, SIGQUIT);
+    sigaction(SIGINT, sig, NULL);
+    sigaction(SIGQUIT, sig, NULL);
+}
+
+void init_sigaction()
+{
+    t_sigaction sig;
+
+    ft_memset2(&sig, 0, sizeof(sig));
+    mask_and_catch_signals(&sig);
+}
